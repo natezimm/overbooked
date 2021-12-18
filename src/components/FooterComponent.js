c5a5e88856aee440929cfb98a569c7eccaa30dd8
Nathan Zimmerman <nathan.a.zimmerman@gmail.com>
2021-12-18 18:15:39 -0500

    Footer colors

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return (
-        <footer className="site-footer" style={{ backgroundImage: `url('/assets/images/reading-fantasy.jpg')`}}>
+        <footer className="site-footer" style={{ backgroundImage: `url('/assets/images/reading.png')`}}>
                 <div className="row">                
                     <div className="col-4 text-center footer-links">
-                        <h5>Links</h5>
+                        <h4>Links</h4>
                         <ul className="list-unstyled">
-                            <li><Link to='/home'>Home</Link></li>
-                            <li><Link to='/library'>Library</Link></li>
-                            <li><Link to='/aboutus'>About</Link></li>
-                            <li><Link to='/contactus'>Contact</Link></li>
+                            <li><Link to='/home' id="home-link">Home</Link></li>
+                            <li><Link to='/library' id="library-link">Library</Link></li>
+                            <li><Link to='/aboutus' id="about-link">About</Link></li>
+                            <li><Link to='/contactus' id="contact-link">Contact</Link></li>
                         </ul>
                     </div>
                     <div className="col-4 text-center footer-social">
-                        <h5>Social</h5>
+                        <h4>Social</h4>
                         <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
